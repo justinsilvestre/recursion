5cf05c854f186ede199b148f9e3c4130d8f12381
Justin Silvestre <justinsilvestre@gmail.com>
2015-03-19 13:38:40 -0500

    got recursive merge sort working

--- mergesort.rb
+++ mergesort.rb
@@ -1,56 +1,39 @@
-def merge_sort(nums)
+
+def merge_sort_rec(nums)
 	num_chunks = nums.map { |n| [n] }
-	p num_chunks
-	result = []
-	# merging
-	while num_chunks.length > 1
-		puts "there are still at least two chunks"
-		(0...num_chunks.length).step(2) do |i|
-			puts "====step #{i}===="
-			chunk = num_chunks[i]
-			next_chunk = num_chunks[i+1]
-			puts "comparing #{chunk} and #{next_chunk}"
-			if next_chunk.nil?
-				puts "only one chunk #{chunk}, nothing to compare"
-				merged_chunks = [chunk]
-			elsif chunk.nil?
-				puts "only one chunk #{next_chunk}, nothing to compare"
-				merged_chunks = [next_chunk]
-			else
-				merged_chunks = []
-				until chunk.empty? && next_chunk.empty?
-					if next_chunk.empty?
-						puts "next chunk is empty"
-						lower_first = chunk.shift
-					elsif chunk.empty?
-						puts "chunk is empty"
-						lower_first = next_chunk.shift
-					else
-						puts "comparing #{chunk} and #{next_chunk}"
-						if chunk.first < next_chunk.first
-							lower_first = chunk.shift
-						else
-							lower_first = next_chunk.shift
-						end
-					end
-					puts "the lowest first number of any chunk is #{lower_first}"
-					merged_chunks << lower_first
-					num_chunks[i] = merged_chunks
-					num_chunks[i+1] = []
-					puts "the combined chunks are #{merged_chunks}"
-				end
-			p num_chunks
-			num_chunks.select! {|n| !(n.empty?) }
-		end
-		result << merged_chunks
-		p result
-		p num_chunks
-	end
-	end
-	p result
+	merge_chunks(num_chunks)
 end
 
+def merge_chunks(all_chunks)
+	return all_chunks[0] unless all_chunks.length > 1
+	sorted_chunks = []
+	all_chunks.each_slice(2) do |chunks|
+		merged_chunks = merge_pair(chunks)
+		sorted_chunks << merged_chunks
+	end
+	merge_chunks(sorted_chunks)
+end
 
+def merge_pair(chunks)
+	first = chunks[0]
+	second = chunks[1]
+	return first if second.nil?
+	result = []
+	until first.empty? && second.empty?
+		if first.empty?
+			lower_first = second.shift
+		elsif second.empty?
+			lower_first = first.shift
+		elsif first.first < second.first
+			lower_first = first.shift
+		else
+			lower_first = second.shift
+		end
+		result << lower_first
+	end
+	result
+end
 
-roo = [8,6,4,7,2,3,9,1]
-merge_sort roo
+roo = [8,6,4,7,2,3,9,1,4]
+
+p merge_sort_rec roo
