07c6403b9ae5492d8f4a8ddbc95936a2767a7ca8
Justin Silvestre <justinsilvestre@gmail.com>
2015-03-19 10:26:43 -0500

    got merge sort working with loops

--- mergesort.rb
+++ mergesort.rb
@@ -3,36 +3,49 @@ def merge_sort(nums)
 	p num_chunks
 	result = []
 	# merging
-	(0...nums.length).step(2) do |i|
-		puts "====step #{i}===="
-		chunk = num_chunks[i]
-		next_chunk = num_chunks[i+1]
-		if next_chunk.nil?
-			puts "only one chunk #{chunk}, nothing to compare"
-			merged_chunks = [chunk]
-		else
-			merged_chunks = []
-			until chunk.empty? && next_chunk.empty?
-				if next_chunk.empty?
-					puts "next chunk is empty"
-					lower_first = chunk.shift
-				elsif chunk.empty?
-					puts "chunk is empty"
-					lower_first = next_chunk.shift
-				else
-					puts "comparing #{chunk} and #{next_chunk}"
-					if chunk.first < next_chunk.first
+	while num_chunks.length > 1
+		puts "there are still at least two chunks"
+		(0...num_chunks.length).step(2) do |i|
+			puts "====step #{i}===="
+			chunk = num_chunks[i]
+			next_chunk = num_chunks[i+1]
+			puts "comparing #{chunk} and #{next_chunk}"
+			if next_chunk.nil?
+				puts "only one chunk #{chunk}, nothing to compare"
+				merged_chunks = [chunk]
+			elsif chunk.nil?
+				puts "only one chunk #{next_chunk}, nothing to compare"
+				merged_chunks = [next_chunk]
+			else
+				merged_chunks = []
+				until chunk.empty? && next_chunk.empty?
+					if next_chunk.empty?
+						puts "next chunk is empty"
 						lower_first = chunk.shift
-					else
+					elsif chunk.empty?
+						puts "chunk is empty"
 						lower_first = next_chunk.shift
+					else
+						puts "comparing #{chunk} and #{next_chunk}"
+						if chunk.first < next_chunk.first
+							lower_first = chunk.shift
+						else
+							lower_first = next_chunk.shift
+						end
 					end
+					puts "the lowest first number of any chunk is #{lower_first}"
+					merged_chunks << lower_first
+					num_chunks[i] = merged_chunks
+					num_chunks[i+1] = []
+					puts "the combined chunks are #{merged_chunks}"
 				end
-				puts "the lowest first number of any chunk is #{lower_first}"
-				merged_chunks << lower_first
-				puts "the combined chunks are #{merged_chunks}"
-			end
+			p num_chunks
+			num_chunks.select! {|n| !(n.empty?) }
 		end
 		result << merged_chunks
+		p result
+		p num_chunks
+	end
 	end
 	p result
 end
